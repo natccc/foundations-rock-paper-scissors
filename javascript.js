adf2519af63910c992adab7160140179aa7f8122
Natalie <natchc@gmail.com>
2025-08-05 22:09:47 +0100

    Work on the logic and it is working fine

---
+++ javascript.js
@@ -0,0 +1,71 @@
+function getComputerChoice() {
+  // return one of 'rock', 'paper','scissors'
+  //use math.random to divide number into three categories
+  const number = Math.floor(Math.random() * 9);
+  if (number <= 2) return "rock";
+  else if (number <= 5) return "paper";
+  else return "scissors";
+}
+
+function getHumanChoice() {
+  return prompt("Choice: ");
+}
+
+
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    function playRound(humanChoice, computerChoice) {
+      humanChoice = humanChoice.toLowerCase();
+
+      //human choose rock
+      if (humanChoice === computerChoice) {
+        console.log("Tie!");
+        return;
+      }
+
+      if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+          console.log("You win! Rock beats Scissors");
+          humanScore++;
+          return;
+        } else {
+          console.log("You lose! Paper beats Rock");
+          computerScore++;
+          return;
+        }
+      } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+          console.log("You win! Paper beats Rock");
+          humanScore++;
+          return;
+        } else {
+          console.log("You lose! Scissors beats Paper");
+          computerScore++;
+          return;
+        }
+      } else if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+          console.log("You win! Paper beats Scissors");
+          humanScore++;
+          return;
+        } else {
+          console.log("You lose! Paper beats Rock");
+          computerScore++;
+          return;
+        }
+      }
+    }
+    
+
+  for (let i = 0; i < 5; i++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  }
+    console.log(`Computer score: ${computerScore}`)
+    console.log(`Your score: ${humanScore}`)
+}
+playGame()
